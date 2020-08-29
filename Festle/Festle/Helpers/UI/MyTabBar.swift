ec0f9816e92dfcf7ab4f41803f91717495806e59
jake33rus <jeny@ulanoff.ru>
2020-08-29 16:51:47 +0300

    Delete Scene Delegate
    all okey!

---
+++ Festle/Festle/Helpers/UI/MyTabBar.swift
@@ -0,0 +1,29 @@
+//
+//  MyTabBar.swift
+//  Festle
+//
+//  Created by Евгений Уланов on 29.08.2020.
+//  Copyright © 2020 Евгений Уланов. All rights reserved.
+//
+
+import UIKit
+
+class MyTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 1
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
